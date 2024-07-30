558e40cb9da1c97ccb8f0fd929e70708ce17cef8
Maoyin Sun <simonmysun@gmail.com>
2024-07-30 13:12:51 +0200

    support char by char output with paginator

--- plugins/paginator/50_pre_output.sh
+++ plugins/paginator/50_pre_output.sh
@@ -6,17 +6,20 @@ if [[ -t 1 ]]; then
 
   exec 3< <(cat - | stdbuf -o0 fold -w $(tput cols) -s);
 
-  LINE_NUM=0;
+  LINE_NUM=1;
   PAGE_SIZE=$(tput lines);
-  while IFS= read -r -u 3 line; do
-    if [[ ${LINE_NUM} -ge ${PAGE_SIZE} ]]; then
-      read -n 1 -s -r -p "Press any key to continue" < /dev/tty;
-      tput el1;
-      echo -ne "\r";
-      LINE_NUM=0;
+  while IFS= read -r -u 3 -N 1 char; do
+    echo -ne "${char}";
+    if [[ "x${char}" == $'x\n' ]]; then
+      LINE_NUM=$((LINE_NUM+1));
+      if [[ ${LINE_NUM} -eq ${PAGE_SIZE} ]]; then
+        read -n 1 -s -r -p "Press any key to continue" < /dev/tty;
+        tput el1;
+        echo -ne "\r";
+        LINE_NUM=1;
+      fi
     fi
-    echo $line;
-    LINE_NUM=$((LINE_NUM+1));
+    sleep 0;
   done
   exec 3>&-;
 else
