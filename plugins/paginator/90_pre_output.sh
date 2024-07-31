6cc8f559c53e3b729e4e0715ae5820c8526c3844
Maoyin Sun <simonmysun@gmail.com>
2024-07-31 09:45:49 +0200

    rewrite fold function for better handling of escape characters. Partially fixes #10

--- plugins/paginator/90_pre_output.sh
+++ plugins/paginator/90_pre_output.sh
@@ -1,7 +1,44 @@
 #!/usr/bin/env bash
 
+fold() {
+  n="${1:-80}";
+  count=0;
+  pos=0;
+  output="";
+
+  while IFS= read -r -N 1 char; do
+    if [[ "x${char}" == $'x\e' ]]; then
+      local esc_seq="${char}";
+      read -r -N 1 char;
+      esc_seq+="$char";
+      if [[ "$char" == "[" ]]; then
+        while true; do
+          read -r -N 1 char;
+          esc_seq+="$char";
+          [[ "$char" =~ [a-zA-Z] ]] && break;
+        done
+      fi
+      output+="$esc_seq";
+    else
+      output+="$char";
+      count=$((count + 1));
+    fi
+
+    echo -ne "$output";
+    output="";
+    if [[ "$char" == $'\n' ]]; then
+      count=0;
+    fi
+    if (( count >= n )); then
+      echo;
+      count=0;
+    fi
+  done
+}
+
+
 if [[ ${TO_TTY} == true ]]; then
-  exec 3< <(cat - | stdbuf -o0 fold -w $(tput cols) -s);
+  exec 3< <(cat - | fold $(tput cols));
 
   LINE_NUM=1;
   PAGE_SIZE=$(tput lines);
@@ -16,7 +53,7 @@ if [[ ${TO_TTY} == true ]]; then
         LINE_NUM=1;
       fi
     fi
-    sleep 0;
+    # sleep 0;
   done
   exec 3>&-;
 else
