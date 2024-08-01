50eb25e51c460d75139bdb5b503a5d4a9123a0f0
Maoyin Sun <simonmysun@gmail.com>
2024-08-01 05:49:15 +0200

    fix escape characters counting issue. resolves #10

--- plugins/paginator/90_pre_output.sh
+++ plugins/paginator/90_pre_output.sh
@@ -1,50 +1,40 @@
 #!/usr/bin/env bash
 
-fold() {
-  n="${1:-80}";
-  count=0;
-  pos=0;
-  output="";
-
+if [[ ${TO_TTY} == true ]]; then
+  COLUMNS=$(tput cols);
+  PAGE_SIZE=$(tput lines);
+  BUFFER="";
+  LENGTH=${COLUMNS};
+  LINE_NUM=1;
   while IFS= read -r -N 1 char; do
     if [[ "x${char}" == $'x\e' ]]; then
-      local esc_seq="${char}";
+      esc_seq="${char}";
       read -r -N 1 char;
-      esc_seq+="$char";
-      if [[ "$char" == "[" ]]; then
+      esc_seq+="${char}";
+      if [[ "x${char}" =~ ^x(\[|\()$ ]]; then
         while true; do
           read -r -N 1 char;
-          esc_seq+="$char";
-          [[ "$char" =~ [a-zA-Z] ]] && break;
+          esc_seq+="${char}";
+          [[ "${char}" =~ [a-zA-Z] ]] && break;
         done
       fi
-      output+="$esc_seq";
+      BUFFER="${BUFFER}${esc_seq}";
     else
-      output+="$char";
-      count=$((count + 1));
+      BUFFER="${BUFFER}${char}";
+      LENGTH=$((LENGTH - 1));
     fi
-
-    echo -ne "$output";
-    output="";
-    if [[ "$char" == $'\n' ]]; then
-      count=0;
-    fi
-    if (( count >= n )); then
-      echo;
-      count=0;
-    fi
-  done
-}
-
-
-if [[ ${TO_TTY} == true ]]; then
-  exec 3< <(cat - | fold $(tput cols));
-
-  LINE_NUM=1;
-  PAGE_SIZE=$(tput lines);
-  while IFS= read -r -u 3 -N 1 char; do
-    echo -ne "${char}";
+    echo -ne "${BUFFER}";
+    BUFFER="";
+    NEWLINE=false;
     if [[ "x${char}" == $'x\n' ]]; then
+      LENGTH=${COLUMNS};
+      NEWLINE=true;
+    elif (( $LENGTH == 0 )); then
+      echo;
+      LENGTH=${COLUMNS};
+      NEWLINE=true;
+    fi
+    if [[ ${NEWLINE} == true ]]; then
       LINE_NUM=$((LINE_NUM+1));
       if [[ ${LINE_NUM} -eq ${PAGE_SIZE} ]]; then
         read -n 1 -s -r -p "Press any key to continue" < /dev/tty;
@@ -53,9 +43,7 @@ if [[ ${TO_TTY} == true ]]; then
         LINE_NUM=1;
       fi
     fi
-    # sleep 0;
   done
-  exec 3>&-;
 else
   logging_debug "Not a terminal";
   cat;
