e4a13dfa36550cad2af834424a51c1b59cec240f
Maoyin Sun <simonmysun@gmail.com>
2024-08-01 21:45:50 +0200

    acquire current cursor position to allow correct folding when not starting from beginning

--- plugins/paginator/90_pre_output.sh
+++ plugins/paginator/90_pre_output.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env bash
 
+get_current_column() {
+  # https://stackoverflow.com/questions/2575037/how-to-get-the-cursor-position-in-bash
+  exec < /dev/tty
+  oldstty=$(stty -g)
+  stty raw -echo min 0
+  # on my system, the following line can be replaced by the line below it
+  echo -en "\033[6n" > /dev/tty
+  # tput u7 > /dev/tty    # when TERM=xterm (and relatives)
+  IFS=';' read -r -d R -a pos
+  stty $oldstty
+  echo $((${pos[1]} - 1))
+}
+
 if [[ ${TO_TTY} == true ]]; then
+  CURR_COL=$(get_current_column);
   COLUMNS=$(tput cols);
   PAGE_SIZE=$(tput lines);
   BUFFER="";
-  LENGTH=${COLUMNS};
+  LENGTH=$((COLUMNS - CURR_COL));
   LINE_NUM=1;
   while IFS= read -r -N 1 char; do
     if [[ "x${char}" == $'x\e' ]]; then
