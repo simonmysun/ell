d52f9defe2420c45cf5ab093b79c015ce752cfd6
Maoyin Sun <simonmysun@gmail.com>
2025-01-29 10:21:50 +0100

    fix wront parameter name in templating test

--- tests/templating.sh
+++ tests/templating.sh
@@ -5,7 +5,7 @@ set -o posix;
 export ELL_TEMPLATE_PATH='/ell/templates/';
 
 echo "openai template test";
-ell --api-style ell_echo -m gpt-4o --api-stream false test;
+ell --api-style ell_echo -m gpt-4o --api-disable-streaming test;
 
 echo "gemini template test";
 ell --api-style ell_echo -t default-gemini test;
